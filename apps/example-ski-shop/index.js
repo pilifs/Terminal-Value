632ae0c0aae4b1c2793f479ce6e3bc7885cdb7fb
Filip Stankovic <filips@gmail.com>
2026-02-14 08:48:55 -0500

    Deprecation warning for back-end server

--- apps/example-ski-shop/index.js
+++ apps/example-ski-shop/index.js
@@ -1,5 +1,12 @@
 import * as Main from './main.js';
 
+console.warn(
+  'THIS FILE IS NOW DEPRECATED. We only use a client side server now for demo from CDN purposes.'
+);
+
+// To view the old CQRS back-end, check out commit: fc119684911d1466defeca0e5409740f709fef64
+// Then run `npm start:ski-shop`
+
 Main.runSimulation().then(() => {
   // Simulation complete
   console.log('Simulation finished.');
