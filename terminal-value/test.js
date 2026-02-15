625927d519580709a202c3b85735b7a0386ba766
Filip Stankovic <filips@gmail.com>
2026-02-15 18:47:00 -0500

    Run experiments with LLM generated views

--- terminal-value/test.js
+++
@@ -1,9 +0,0 @@
-import { generateValueMetadataRealtime } from './coreServices.js';
-
-import { parseValueResults } from './functionalTests/fixedMocks/parseValueResults.js';
-
-const test = generateValueMetadataRealtime(parseValueResults[0]);
-
-debugger;
-
-test;
